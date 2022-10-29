4738737a72bde7a7bf10f54e04935c7846e6e8ff
Shawn <lcorel86@gmail.com>
2022-10-29 00:25:17 -0500

    at stream http example

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-const EventEmitter = require("events");
+const { createReadStream } = require("fs");
 
-const customEmitter = new EventEmitter();
+const stream = createReadStream(
+    "./content/big.txt", 
+    {highWaterMark: 90000, encoding: "utf8"}
+);
 
-customEmitter.on("response", ()=>{
-    console.log(`data recived `);
+// default 64kb
+// last buffer - remainder
+// highWaterMark - control size
+// const stream = createReadStream("./content/big.txt", { highWatermark: 90000 })
+// const stream = createReadStream("../content/big.txt", { encoding: "utf8" })
+
+
+stream.on("data", (result) => {
+    console.log(result);
 })
 
-customEmitter.emit("response");
+stream.on("error", (err) => console.log(err))
